c5c4115d6894ae5f9753ad74c1e06bb4e612ce04
dariagmit <g00411221@gmit.ie>
2022-03-18 22:01:44 +0000

    problem sheet - weekly task 07 (WIP) v3

--- es.py
+++ es.py
@@ -9,24 +9,26 @@ import sys
 
 filename = sys.argv[1]  # takes in command line argument. argv[0] would be a path to this file
 
-letter = 'e' 
-letter2 = letter.upper()  
-# Could be letter2 = 'E'
-# letter 2 = letter.upper() would be more useful if we were reading in user's input for the letter
-
-def countEs(filename, letter):
+def countEs(filename):
     with open(filename) as f:
         text = f.read()
-        numberLower = text.count(letter)   # counts instances of 'e'
-        numberUpper = text.count(letter2)  # counts instances of 'E'
-        totalNumber = numberLower + numberUpper # sums both for total number instances of the letter despite of case
-        
-        return (numberLower, numberUpper, totalNumber)
+        numberLower = 0
+        numberUpper = 0
+        numberTotal = 0
+        for i in text:
+            if i == 'e':
+                numberLower = numberLower + 1   # counts instances of 'e'
+                numberTotal = numberTotal + 1
+            elif i == 'E':
+                numberUpper = numberUpper + 1  # counts instances of 'E'
+                numberTotal = numberTotal + 1
+               
+        return (numberLower, numberUpper, numberTotal)
 
-values = countEs (filename, letter)   # function countES () returns multiple values as a tuple
+values = countEs (filename)   # function countES () returns multiple values as a tuple
 
 lower = values [0] # storing each value as a separte variable for use in message
 upper = values [1]
 total = values [2]
 
-print (f'This file contains total of {total} instances of letter "e" {lower} of witch are lowercase and {upper} are uppercase')
+print (f'This file contains total of {total} instances of letter "e" {lower} of which are lowercase and {upper} are uppercase')
