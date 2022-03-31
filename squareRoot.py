f139871b08ed6279fc8ded99b244f69540dec3fa
dariagmit <g00411221@gmit.ie>
2022-03-31 16:15:17 +0100

    problem sheet - weekly task 06 v2

--- squareRoot.py
+++ squareRoot.py
@@ -1,26 +1,63 @@
 # Problem Sheet - Weekly Task 06
 
-# Program that takes a positive floating-point number as input 
-# and outputs an approximation of its square root.
+# Program that takes a positive floating-point number as input and outputs an approximation of its square root.
 # Program uses the newton method of estimating square roots.
 
 # Author: Daria Sep
 
 
-
 def sqrt(n):        
     approx = 0.5 * n    # root of n can't be less than half of the its value, we can therfore start approximating here
-    better = 0.5 * (approx + (n / approx))      # Newton Method sqyare root approximation formula
+    better = 0.5 * (approx + (n / approx))      # Newton Method square root approximation formula
     while better != approx :    # as long as closer approximation can be found, the program will keep running  
         approx = better         # initial approximation is being replaced with better approximation
         better = 0.5 * (approx + (n / approx))      # calculations continue
     return approx
 
-n = float (input ('Please enter a positive number: '))   # User input for n
-rounding = int (input ('Enter the number of decimals for the answer rounding: ')) # user input for rounding
-roundedAnswer = round ((sqrt(n)), rounding)  # Rounding the number to the decimal places defined by user in the line above
+print ('Please enter a positive number: ', end = '' )
+# catching invalid inputs and prompting user to input another value
+while True:
+    try: 
+        n = float (input (''))   # User input for n
+        if n == '' or n <= 0:
+            raise ValueError ('Incorrect input.')
+        break
+    except ValueError:
+        print (f'This is not a positive number. Try again: ', end ='')
 
-print (f'The approximate root of {n} rounded to {rounding} decimal place(s) is {roundedAnswer}') 
+rawResult = (sqrt (n))
+print (f'The approximate square root of {n} is {rawResult}')
 
+# extra: offering option of rounding the result to the chosen number of decimal places
+print ('Do you want to round the result?', end = ' ')
+while True: 
+    try: 
+        rounding = input ('Press Y for \'yes\' \ N or leave blank for \'no\': ')
 
-# WIP - add error handling, fix comments. 
+        if rounding == 'Y' or rounding == 'y': 
+            print ('Enter the number of decimals for the answer rounding: ', end = '')
+            while True: 
+                try:
+                    r = int (input ('')) # user input for rounding
+                    if r < 0:
+                        raise ValueError ('Number of decimals cannot be negative.')
+                    break
+                except ValueError:
+                        print ('Invalid input. Please input a positive integer: ', end = '')
+            
+            if r == 0: 
+                rAnswer = round (sqrt(n))
+                print (f'The approximate square root of {n} rounded to the closest whole number is {round(rAnswer)}') 
+            else: 
+                rAnswer = round ((sqrt(n)), r)
+                print (f'The approximate square root of {n} rounded to {r} decimal place(s) is {rAnswer}')
+            break
+
+        if rounding == 'N' or rounding == 'n' or rounding == '':
+            print ('Ok, that\'s it then. See you later :)')
+            quit
+        else: 
+            raise ValueError ('Invalid input.')
+        break
+    except ValueError: 
+        print ('Invalid input.')
