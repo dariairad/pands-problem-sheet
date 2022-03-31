ecd2abaeb7f6615f02e35b5e79f278fb86403db8
dariagmit <g00411221@gmit.ie>
2022-03-31 11:54:53 +0100

    problem sheet - weekly task 04 - final version

--- collatz.py
+++ collatz.py
@@ -1,27 +1,27 @@
 # Problem Sheet - Weekly Task 04
 
 # Program that asks the user to input any positive integer and outputs the successive values of the following calculation.
-# At each step program calculates the next value by taking the current value and, 
-# if it is even, dividing it by two, 
-# but if it is odd, multiplying it by three and adding one.
+# At each step program calculates the next value by taking the current value and, if it is even, dividing it by two, but if it is odd, multiplying it by three and adding one.
 # Program ends if the current value is one.
 
 # Author: Daria Sep
 
-number = int (input ('Please input a number: '))
+while True:     # To prompt another input request in case of value error
+    try:        # try/except to catch exceptions (0, negative, blank or non-int input)
+        number = int (input ('Please input a positive integer: '))
+        if number <= 0 or number == '':
+            raise ValueError ('Incorrect value.')
+        break
+    except ValueError:
+        print ('Incorrect input. Please try again.')
 
-while number != 1: 
+while number != 1:  # to continue calculations until number is equal to 1
 
-    print(number, end = ' ')  
-    # ref: https://www.geeksforgeeks.org/gfact-50-python-end-parameter-in-print/
-    
+    print(number, end = ' ')  # to return each calculations output, and keep them all in one line
+        
     if (number % 2) == 0:
         number = int (number / 2)
     else:
         number = int ((number * 3) + 1)
-
-print (number)
-
-# WIP -> add ValueError
-
-
+    
+print (number) # inlcues last output (1) 
