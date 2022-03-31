01d14c6bae209f6bda83ff92bda0c8f420a61870
dariagmit <g00411221@gmit.ie>
2022-03-31 12:13:50 +0100

    problem sheet - weekly task 05 - final version

--- weekday.py
+++ weekday.py
@@ -4,21 +4,15 @@
 
 # Author: Daria Sep
 
-# ref: 
-# https://www.geeksforgeeks.org/python-datetime-module/
-# https://docs.python.org/3/library/datetime.html?highlight=datetime%20module 
 
+from datetime import date       # Extracting today's date
 
-from datetime import date
-# Extracing today's date
+# today() - returns the current local date.
+# weekday() - returns the day of the week as integer where Monday is 0 and Sunday is 6.
+# Therefore for int < 5 ie.from 0 to 4, it's a weekday (Mon-Fri).
 
 if date.today().weekday() < 5:
-    print('Yes, unfortunately today is a weekday')
-
-# today() - returns the current local date
-# weekday() - returns the day of the week as integer where Monday is 0 and Sunday is 6
-# for < 5 it's weekday (mon-fri)
-
+    print('Yes, unfortunately today is a weekday.')
 else:
     print ('It is the weekend, yay!')
 
