98d8ef070331bda6bf7d6b3b4743cd982197f118
dariagmit <g00411221@gmit.ie>
2022-04-06 21:50:02 +0100

    syntax formatting

--- plottask.py
+++ plottask.py
@@ -7,9 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 xpoints = np.array(range (0, 5))
-ypointsf = xpoints              # f(x)=x
-ypointsg = xpoints * xpoints    # g(x)=x2
-ypointsh = xpoints ** 3         # h(x)=x3
+ypointsf = xpoints              
+ypointsg = xpoints * xpoints    
+ypointsh = xpoints ** 3         
 
 plot.plot(xpoints, ypointsf, label='f(x)=x', color='red')
 plot.plot(xpoints, ypointsg, label='g(x)=x\N{SUPERSCRIPT TWO}', color='green')
